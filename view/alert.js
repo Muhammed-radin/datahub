36978f7d8cc62f5092e4491a905bcb8d0a93bcfd
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-06-07 09:48:28 +0530

    alert blocker added

--- view/alert.js
+++ view/alert.js
@@ -4,10 +4,12 @@ var _alertIndex = 0
 
 function alert(head = 'Untitled', msg = '') {
   var _id = 'ALERT_ID_' + Math.floor(Math.random() * 99999)
-  var html = `<div class="alert-modal" id="${_id}">
+  var html = `
+  <div class="block-div bg-ogrey" id="${_id}_BLOCK"></div>
+  <div class="alert-modal" id="${_id}">
     <div class="alert-head">${head}</div>
     <div class="alert-body">${msg}</div>
-    <button class="alert-right" onclick="document.getElementById('${_id}').remove();_alerting = false;resolveAlert()">CONTINUE</button>
+    <button class="alert-right" onclick="document.getElementById('${_id}').remove();document.getElementById('${_id}_BLOCK').remove();_alerting = false;resolveAlert()">CONTINUE</button>
   </div>`
 
 
@@ -19,6 +21,7 @@ function alert(head = 'Untitled', msg = '') {
     })
   } else {
     document.body.innerHTML += (html)
+    document.getElementById(_id+'_BLOCK').style.display = 'block';
     _alerting = true
   }
 
